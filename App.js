4845114fcd0a525cba0857d33072bedb6435be8f
unknown <rajeshkumarchoudhary096@gamil.com>
2024-01-21 01:21:07 +0530

    Functional Component with Composition of component

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React.createElement => object => HTMLElement(after render)
-const heading = React.createElement("h1", {id:"heading"}, "Hello React World!");
-console.log(heading);
+// Functional Component - Which return some JSX element
+const Title = () => {
+    return <h1 id="title">Explore-React Title</h1>
+};
 
-// JSX it is not HTML in js but it HTML-like or XML-like syntax
-// React App can be build without using JSX but JSX is Development friendly easy to read code.
+// If we are returning signgle line then we can write in sigle line this syntax is also valid.
+//const Title1 = () => <h1 id="title">Explore-React Title1</h1>;
 
-const jsxHeading = <h1 id="heading">Hello Explore React</h1>
-console.log(jsxHeading);
+// Composition of Component - When we use one component inside another is called Composition of componnent.
+const HeadingComponent = () => {
+    return (<div id="headingcomponent">
+        <Title />
+        <h1 id="heading">Composition of Title component into Heading Component</h1>
+    </div>)
+}
 
-//Behind the scene both are object only even jsx syntax used to create react element only.
+console.log(HeadingComponent);
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent/>);
